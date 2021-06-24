582f07d1d56b288d34a49073a51bc1419755df1a
iamtrazy <iamtrazy@protonmail.com>
2021-06-25 02:21:25 +0530

    Update UserManager.sh

--- etc/UserManager.sh
+++ etc/UserManager.sh
@@ -2,6 +2,6 @@
 echo -ne "Enter the username : "; read username
 echo -ne "Enter password (please use a strong password) : "; read password
 echo -ne "Enter No. of Days till expiration : ";read nod
-exd=$(date +%F  -d "$nod days")
+exd=$(date +%F  -d "+$nod days")
 useradd -e $exd -M -N -s /bin/false $username && echo "$username:$password" | chpasswd && echo User $username added to the system successfully. Press enter key to return to main menu . ;read || echo Failed to add user $username please try again.
 echo returned
