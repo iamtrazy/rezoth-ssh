93ecca457173dbaa5cb5f2779b23d82f6cd31b56
iamtrazy <iamtrazy@protonmail.com>
2021-06-28 12:50:56 +0530

    Update UserManager.sh

--- etc/UserManager.sh
+++ etc/UserManager.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-#font colors
-
 RED="\e[31m"
 GREEN="\e[32m"
 YELLOW="\e[33m"
 ENDCOLOR="\e[0m"
-
-#add users
 
 echo -ne "Enter the username : "; read username
 while true; do
@@ -28,7 +24,5 @@ echo -e "${GREEN}\nUsername :${YELLOW} $username" &&
 echo -e "${GREEN}\nPassword :${YELLOW} $password" &&
 echo -e "${GREEN}\nExpire Date :${YELLOW} $exd ${ENDCOLOR}" ||
 echo -e "${RED}\nFailed to add user $username please try again.${ENDCOLOR}"
-
-#return to panel
 
 echo -e "\nPress Enter key to return to main menu"; read
